fb7339d76920ff71411cb3913f7751b4a2070530
jtnash4295616 <jeremiah.nash@gmail.com>
2016-05-30 15:56:47 -0700

    finished with static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  # This is my first test line
+  config.include Capybara::DSL
+  
 end
